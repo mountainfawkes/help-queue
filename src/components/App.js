1e331cafe916b21611262d9a0f25c833c403db2b
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-06-27 19:34:37 -0700

    adds ticket and ticket list components

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,10 @@
 import React from "react";
+import Header from "./Header";
 
 function App() {
-  const name = "Thato";
-  const name2 = "Haley";
   return (
   <React.Fragment>
-    <h1>Help Queue</h1>
-    <h3>3a</h3>
-    <h3>{name} and {name2}</h3>
-    <p><em>Firebase entries not saving!</em></p>
-    {/* This is a comment! */}
-    <hr/>
+    <Header />
   </React.Fragment>
   );
 }
---
+++ src/components/Ticket.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Ticket() {
+  const name = "Thato";
+  const name2 = "Haley";
+  return (
+  <h3>3a</h3>
+  <h3>{name} and {name2}</h3>
+  <p><em>Firebase entries not saving!</em></p>
+  {/* This is a comment! */}
+  <hr/>
+  );
+}
+
+export default Ticket;
