d8b396a828641e902cc8bfab1d16ecb6f5c7ded8
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-06-27 20:26:00 -0700

    adds ticket image to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
 import React from "react";
+import helpTicketImg from "./../img/ticket.png";
 
 function Header() {
   return (
     <h1>Help Queue</h1>
+    <img src={helpTicketImg} alt="save our souls (as an image)" />
   );
 }
 
