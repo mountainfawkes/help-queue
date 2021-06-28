158c5f7df03655b2e3a896c0b29e911ab766e9a8
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-06-27 20:25:02 -0700

    fixes artifact code from early lessons

--- src/components/Ticket.js
+++ src/components/Ticket.js
@@ -2,13 +2,12 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function Ticket(props) {
-  const name = "Thato";
-  const name2 = "Haley";
   return (
-  <h3>3a</h3>
-  <h3>{props.location} and {props.names}</h3>
-  <p><em>{props.issue}</em></p>
-  <hr/>
+    <React.Fragment>
+      <h3>{props.location} -- {props.names}</h3>
+      <p><em>{props.issue}</em></p>
+      <hr/>
+    </React.Fragment>
   );
 }
 
