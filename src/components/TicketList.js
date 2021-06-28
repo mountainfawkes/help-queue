5a088a63bdc0b827fd411551992d519bf7af9fb3
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-06-27 19:59:16 -0700

    adds additional ticket 3B

--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -7,6 +7,10 @@ function TicketList() {
       location="3A"
       names="Thato and Haley"
       issue="Firebase will not save the record!" />
+    <Ticket
+      location="3B"
+      names="Sleater and Kinney"
+      issue="Prop types are throwing an error" />
   );
 }
 
