9fd33f8c1d3dc099cb014347af65c073b8a979d9
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-06-27 20:12:29 -0700

    adds ticket list and maps iterations to the DOM

--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -1,16 +1,34 @@
 import React from "react";
 import Ticket from "./Ticket";
 
+const masterTicketList = [
+  {
+    location: "3A",
+    names: "Thato and Haley",
+    issue: "Firebase will not save the record!"
+  },
+  {
+    location: "3B",
+    names: "Sleater and Kinney",
+    issue: "Prop types are throwing an error"
+  }
+  {
+    location: "9F",
+    names: "Imani and Jacob",
+    issue: "Child component is not rendering"
+  }
+]
+
 function TicketList() {
   return (
-    <Ticket
-      location="3A"
-      names="Thato and Haley"
-      issue="Firebase will not save the record!" />
-    <Ticket
-      location="3B"
-      names="Sleater and Kinney"
-      issue="Prop types are throwing an error" />
+    <React.Fragment>
+    {masterTicketList.map((ticket, index) =>
+      <Ticket names = {ticket.names}
+        location = {ticket.location}
+        issue = {ticket.issue}
+        key = {index} />
+    )}
+    </React.Fragment>
   );
 }
 
