1885e8fbe7d7d677365819140fc675c5af387d14
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-06-27 19:59:29 -0700

    adds PropTypes

--- src/components/Ticket.js
+++ src/components/Ticket.js
@@ -1,15 +1,21 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Ticket() {
+function Ticket(props) {
   const name = "Thato";
   const name2 = "Haley";
   return (
   <h3>3a</h3>
   <h3>{props.location} and {props.names}</h3>
   <p><em>{props.issue}</em></p>
-  {/* This is a comment! */}
   <hr/>
   );
 }
+
+Ticket.propTypes = {
+  location: PropTypes.string,
+  names: PropTypes.string,
+  issue: PropTypes.string
+};
 
 export default Ticket;
