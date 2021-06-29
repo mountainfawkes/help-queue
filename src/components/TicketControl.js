b0599cce127a912139399688075b85d2b6d55dac
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-06-29 07:58:50 -0700

    adds ticket form and ticket control to app

---
+++ src/components/TicketControl.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import NewTicketForm from './NewTicketForm';
+import TicketList from './TicketList';
+
+class TicketControl extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // somethingggggg
+    }
+  }
+
+  render() {
+    return (
+      <>
+      </>
+    );
+  }
+}
+
+export default TicketControl;
