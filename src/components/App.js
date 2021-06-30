d1f94ce047df824ab83f7bf2bd593778221b0444
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-06-30 08:09:14 -0700

    applies style object

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,18 @@ import Header from "./Header";
 import TicketControl from "./TicketControl";
 
 function App() {
+  const styles = {
+    backgroundColor: '#ecf0f1',
+    fontFamily: 'serif',
+    padding: '10 10'
+  }
+
   return (
   <React.Fragment>
-    <Header />
-    <TicketControl />
+    <div style={styles}>
+      <Header />
+      <TicketControl />
+    </div>
   </React.Fragment>
   );
 }
